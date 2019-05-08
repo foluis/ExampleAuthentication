a3a1902d7646a0babd4ea2d49625424c2b538aa6
Luis Fernando Forero G <foluis@hotmail.com>
2019-05-07 19:41:54 -0500

    Add project files.

---
+++ Database/dbo/Tables/TblBook.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TblBook](
+    [BookID] [int] IDENTITY(1,1) NOT NULL,
+    [ISBN] [varchar](50) NULL,
+    [Title] [varchar](100) NULL,
+    [Author] [varchar](50) NULL,
+    [Description] [varchar](200) NULL,
+    [Publisher] [varchar](50) NULL,
+    [PublishedYear] [int] NULL,
+    [Price] [decimal](18, 0) NULL
+	)
---
+++ Database/dbo/Tables/TblUser.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TblUser]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[FullName] [varchar](50) NULL,
+	[Email] [varchar](50) NULL,
+    [Password] [varbinary](128) NULL,
+    [Salt] [varbinary](128) NULL
+)
